518d5bf1e7d324311a2cc1f03b82667377a940ce
Devin Ledwell <devinledwell@gmail.com>
2023-02-25 19:38:45 -0330

    Style Update
    Entry windows created.
    Style added to avoid annoying blank screen.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import { useState } from "react";
-import "./App.css";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
+import "./darkMode.css";
+import "./index.css"
 import MainScreen from "./components/MainScreen";
+import AddGroup from "./components/AddGroup";
+import AddPerson from "./components/AddPerson";
+import EditGroup from "./components/EditGroup";
+import EditPerson from "./components/EditPerson";
 
 function App() {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
   const [showBill, setShowBill] = useState(true);
   const [selectPersonEdit, setSelectPersonEdit] = useState(false);
   const [showPersonEdit, setPersonEdit] = useState(false);
@@ -11,23 +24,141 @@ function App() {
   const [showGroupEdit, setGroupEdit] = useState(false);
   const [showHistory, setHistory] = useState(false);
 
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    document.body.className = theme;
+  }, [theme]);
   return (
-    
     <>
-      <main>
-        
-        {showBill ? (
-          <div>
-            <Header/>
-            <button onClick={() => {setShowBill(false);setSelectPersonEdit(true)}}>Split a Bill</button>
-            <button onClick={() => {setShowBill(false);setPersonEdit(true)}}>Edit Person</button>
-            <button onClick={() => {setShowBill(false);setGroupEdit(true)}}>Edit Group</button>
-            <button onClick={() => {setShowBill(false);setHistory(true)}}>Show History</button>
-          </div>):''}
+      <main className="m-5 p-5 md:max-w-xl md:mx-auto lg:max-w-2xl xl:max-w-4xl bg-white-500 rounded shadow">
+        <div className={`App ${theme}`}>
+          {showBill ? (
+            <div className="flex flex-col items-center justify-center mb-5 xl:flex-row xl:justify-between">
+              <Header
+                showBill={showBill}
+                personEdit={showPersonEdit}
+                selectPersonEdit={selectPersonEdit}
+                setPersonEdit={setPersonEdit}
+                groupEdit={showGroupEdit}
+              ></Header>
+              <ul>
+                <li>
+                  <button
+                    className="justify-center mt-5 ml-5 bg-blue-500 text-white font-bold py-2 px-4 rounded shadow border-2 border-blue-500 hover:bg-transparent hover:text-blue-500 transition-all duration-300"
+                    onClick={() => {
+                      setShowBill(false);
+                      setPersonEdit(true);
+                    }}
+                  >
+                    Split a Bill
+                  </button>
+                </li>
+                <li>
+                  <button
+                    className="mt-5 ml-5 bg-blue-500 text-white font-bold py-2 px-4 rounded shadow border-2 border-blue-500 hover:bg-transparent hover:text-blue-500 transition-all duration-300"
+                    onClick={() => {
+                      setShowBill(false);
+                      setSelectPersonEdit(true);
+                    }}
+                  >
+                    Edit Person
+                  </button>
+                </li>
+                <li>
+                  <button
+                    className="mt-5 ml-5 bg-blue-500 text-white font-bold py-2 px-4 rounded shadow border-2 border-blue-500 hover:bg-transparent hover:text-blue-500 transition-all duration-300"
+                    onClick={() => {
+                      setShowBill(false);
+                      setGroupEdit(true);
+                    }}
+                  >
+                    Edit Group
+                  </button>
+                </li>
+                <li>
+                  <button
+                    className="mt-5 ml-5 bg-blue-500 text-white font-bold py-2 px-4 rounded shadow border-2 border-blue-500 hover:bg-transparent hover:text-blue-500 transition-all duration-300"
+                    onClick={() => {
+                      setShowBill(false);
+                      setHistory(true);
+                    }}
+                  >
+                    Show History
+                  </button>
+                </li>
+                <li>
+                  <button
+                    onClick={toggleTheme}
+                    className="mt-5 ml-5 bg-black text-white font-bold py-2 px-4 rounded shadow border-2 border-black hover:text-white-500 transition-all duration-300"
+                  >
+                    Toggle Mode
+                  </button>
+                </li>
+              </ul>
+            </div>
+          ) : (
+            ""
+          )}
+          {showPersonEdit ? (
+            <div>
+              <Header
+                showBill={showBill}
+                setShowBill={setShowBill}
+                personEdit={showPersonEdit}
+                groupEdit={showGroupEdit}
+                setPersonEdit={setPersonEdit}
+                setGroupEdit={setGroupEdit}
+              ></Header>
+              <ul>
+                <li>
+                  <a href="blank">John</a>
+                </li>
+                <li>
+                  <a href="blank">Jacob</a>
+                </li>
+                <li>
+                  <a href="blank">Jim</a>
+                </li>
+                <li>
+                  <a href="blank">Jack</a>
+                </li>
+              </ul>
+              <button
+                className="mt-5 ml-5 bg-blue-500 text-white font-bold py-2 px-4 rounded shadow border-2 border-blue-500 hover:bg-transparent hover:text-blue-500 transition-all duration-300"
+                onClick={() => setAddPerson(true)}
+              >
+                Add a Person
+              </button>
+              <ul>
+                <li>
+                  <a href="blank">Household</a>
+                </li>
+                <li>
+                  <a href="blank">Family</a>
+                </li>
+                <li>
+                  <a href="blank">Work</a>
+                </li>
+              </ul>
+              <button className="mt-5 ml-5 bg-blue-500 text-white font-bold py-2 px-4 rounded shadow border-2 border-blue-500 hover:bg-transparent hover:text-blue-500 transition-all duration-300">
+                Add a group
+              </button>
+            </div>
+          ) : (
+            ""
+          )}
           {selectPersonEdit ? (
             <div>
-            <Header/>
-              <button onClick={() => {setShowBill(true);setSelectPersonEdit(false)}}>BACK</button>
+              <Header
+                showBill={showBill}
+                setShowBill={setShowBill}
+                personEdit={showPersonEdit}
+                selectPersonEdit={selectPersonEdit}
+                setSelectPersonEdit={setSelectPersonEdit}
+                groupEdit={showGroupEdit}
+                setPersonEdit={setPersonEdit}
+                setGroupEdit={setGroupEdit}
+              ></Header>
               <ul>
                 <li>
                   <a href="blank">John</a>
@@ -42,43 +173,26 @@ function App() {
                   <a href="blank">Jack</a>
                 </li>
               </ul>
-              <button onClick={() => setAddPerson(true)}>Add a Person</button>
-              <ul>
-                <li>
-                  <a href="blank">Household</a>
-                </li>
-                <li>
-                  <a href="blank">Family</a>
-                </li>
-                <li>
-                  <a href="blank">Work</a>
-                </li>
-              </ul>
-              <button>Add a group</button>
-              <div>
-                <button onClick={() => setShowBill(true)}>Home</button>
-              </div>
+              <button
+                className="mt-5 ml-5 bg-blue-500 text-white font-bold py-2 px-4 rounded shadow border-2 border-blue-500 hover:bg-transparent hover:text-blue-500 transition-all duration-300"
+                onClick={() => setAddPerson(true)}
+              >
+                Edit Person
+              </button>
             </div>
-            ): ''}
-        {showPersonEdit ? (
+          ) : (
+            ""
+          )}
+          {showGroupEdit ? (
             <div>
-            <Header/>
-              <button onClick={() => {setShowBill(true);setPersonEdit(false)}}>BACK</button>
-              <ul>
-                <li>
-                  <a href="blank">John</a>
-                </li>
-                <li>
-                  <a href="blank">Jacob</a>
-                </li>
-                <li>
-                  <a href="blank">Jim</a>
-                </li>
-                <li>
-                  <a href="blank">Jack</a>
-                </li>
-              </ul>
-              <button onClick={() => setAddPerson(true)}>Edit Person</button>
+              <Header
+                showBill={showBill}
+                setShowBill={setShowBill}
+                personEdit={showPersonEdit}
+                groupEdit={showGroupEdit}
+                setPersonEdit={setPersonEdit}
+                setGroupEdit={setGroupEdit}
+              ></Header>
               <ul>
                 <li>
                   <a href="blank">Household</a>
@@ -90,40 +204,30 @@ function App() {
                   <a href="blank">Work</a>
                 </li>
               </ul>
-              <button>Edit Group</button>
-              <div>
-                <button onClick={() => setShowBill(true)}>Home</button>
-              </div>
+              <button className="mt-5 ml-5 bg-blue-500 text-white font-bold py-2 px-4 rounded shadow border-2 border-blue-500 hover:bg-transparent hover:text-blue-500 transition-all duration-300">
+                Edit group
+              </button>
             </div>
-            ): ''}
-        {showGroupEdit ? (
-          <div>
-          <Header/>
-          <button onClick={() => {setShowBill(true);setGroupEdit(false)}}>BACK</button>
-          <ul>
-          <li>
-            <a href="blank">Household</a>
-          </li>
-          <li>
-            <a href="blank">Family</a>
-          </li>
-          <li>
-            <a href="blank">Work</a>
-          </li>
-        </ul>
-        <button>Edit group</button>
+          ) : (
+            ""
+          )}
+          {showHistory ? (
+            <div>
+              <Header
+                showBill={showBill}
+                setShowBill={setShowBill}
+                setHistory={setHistory}
+                showHistory={showHistory}
+              ></Header>
+
+              <article>
+                <p>Show history of past few weeks</p>
+              </article>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
-        ) : ''}
-        {showHistory ? (
-          <div>
-          <Header/>
-          <button onClick={() => {setShowBill(true);setHistory(false)}}>BACK</button>
-          
-          <article>
-            <p>Show history of past few weeks</p>
-            </article>
-        </div>
-        ) : ''}
       </main>
     </>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,101 @@
 import React from "react";
-import { useState } from "react";
+import { IoIosArrowBack } from "react-icons/io";
 
-export default function Header({ setShowBill, setPersonEdit, setGroupEdit }) {
-    setShowBill = useState(false);
-    setPersonEdit = useState(false);
-    setGroupEdit= useState(false);
+export default function Header({
+  showBill,
+  setShowBill,
+  personEdit,
+  setPersonEdit,
+  setSelectPersonEdit,
+  selectPersonEdit,
+  setGroupEdit,
+  groupEdit,
+  setHistory,
+  showHistory,
+}) {
+  console.log(showBill);
+  console.log(personEdit);
+  console.log(groupEdit);
   return (
     <>
-      {setShowBill ? (<h1>Bill Splitter</h1>):''}
-      {setPersonEdit ? (<h1>Edit a person or group</h1>):''}
-      {setGroupEdit ? (<h1>Edit group</h1>):''}
+      {showBill ? (
+        <header className="flex flex-col items-center justify-center mb-5 xl:flex-row xl:justify-between">
+          <h1 className="font-bold uppercase tracking-wide text-4xl mb-3">
+            Bill Splitter
+          </h1>
+        </header>
+      ) : (
+        ""
+      )}
+      {personEdit ? (
+        <>
+          <div className="flex flex-col items-center text-4xl mb-5 xs:flex-row xs:justify-center md:flex-row md:justify-center xl:flex-row xl:justify-center">
+            <button
+              onClick={() => {
+                setShowBill(true);
+                setPersonEdit(false);
+              }}
+            >
+              <IoIosArrowBack />
+            </button>
+            <h1>Select Person or Group</h1>
+          </div>
+        </>
+      ) : (
+        ""
+      )}
+
+      {setSelectPersonEdit ? (
+        <>
+          <div className="flex flex-col items-center text-4xl mb-5 xs:flex-row xs:justify-center md:flex-row md:justify-center xl:flex-row xl:justify-center">
+            <button
+              onClick={() => {
+                setShowBill(true);
+                setSelectPersonEdit(false);
+              }}
+            >
+              <IoIosArrowBack />
+            </button>
+            <h1>Edit Person</h1>
+          </div>
+        </>
+      ) : (
+        ""
+      )}
+      {groupEdit ? (
+        <>
+          <div className="flex flex-col items-center text-4xl mb-5 xs:flex-row xs:justify-center md:flex-row md:justify-center xl:flex-row xl:justify-center">
+            <button
+              onClick={() => {
+                setShowBill(true);
+                setGroupEdit(false);
+              }}
+            >
+              <IoIosArrowBack />
+            </button>
+            <h1>Edit a group</h1>
+          </div>
+        </>
+      ) : (
+        ""
+      )}
+      {showHistory ? (
+        <>
+          <div className="flex flex-col items-center text-4xl mb-5 xs:flex-row xs:justify-center md:flex-row md:justify-center xl:flex-row xl:justify-center">
+            <button
+              onClick={() => {
+                setShowBill(true);
+                setHistory(false);
+              }}
+            >
+              <IoIosArrowBack />
+            </button>
+            <h1>History</h1>
+          </div>
+        </>
+      ) : (
+        ""
+      )}
     </>
   );
 }
