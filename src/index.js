a0a4d11474b39f57c30211298ad3ce3adbf00e44
Devin Ledwell <devinledwell@gmail.com>
2024-02-06 21:49:31 -0330

    Changes

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ Amplify.configure(awsExports);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-    <App />
+    <HashRouter basename='/'>
+          <App />
     </HashRouter>
   </React.StrictMode>
 );
