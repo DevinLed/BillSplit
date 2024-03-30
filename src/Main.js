5baea27bcb7b8b86280c0fcd3554d79480a08437
Devin Ledwell <devinledwell@gmail.com>
2024-03-30 19:55:08 -0230

    Pathing update for landing page sections

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import App from './App';
-import LandingPage from './components/LandingPage';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import App from "./App";
+import LandingPage from "./components/LandingPage";
+import AboutMe from "./components/Aboutme";
+import Tutorial from "./components/Tutorial";
+import Contact from "./components/Contact";
 
 const Main = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+
+      <Route path="/Contact" element={<Contact />} />
+      <Route path="/Tutorial" element={<Tutorial />} />
+      <Route path="/AboutMe" element={<AboutMe />} />
       <Route path="/App/*" element={<App />} />
     </Routes>
   );
