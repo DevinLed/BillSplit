c8b753267ebda11b7e9d16406e4d606991906bbf
Devin Ledwell <devinledwell@gmail.com>
2024-04-27 22:57:54 -0230

    dataThrow list rework

--- src/components/SplitBill.js
+++ src/components/SplitBill.js
@@ -8,6 +8,7 @@ import { CSSTransition } from "react-transition-group";
 import { API } from "aws-amplify";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@material-tailwind/react";
+import MethodSelect from "./MethodSelect";
 
 export default function SplitBill({
   addPerson,
@@ -45,17 +46,33 @@ export default function SplitBill({
   selectSelf,
   selfExpense,
   setSelfExpense,
+  setPersonReceiptAmount,
 }) {
+  const [selectPersonReceipt, setSelectPersonReceipt] = useState(true);
+  const [selectMethodManual, setSelectMethodManual] = useState(false);
+  const [selectMethodPicture, setSelectMethodPicture] = useState(false);
   const [selectPersonList, setSelectPersonList] = useState(true);
+  const [selectedModalPerson, setSelectedModalPerson] = useState();
   const selfContactId = user.attributes.sub;
   const [selfAdded, setSelfAdded] = useState(false);
-
+  const [showMethodSelectModal, setShowMethodSelectModal] = useState(false);
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
 
   const handleNavigation = async () => {
     navigate(`/App/ReceiptInput/${user.attributes.sub}`);
   };
+
+  const isEmptyDataThrow =
+    !dataThrow ||
+    dataThrow.filter(
+      (item) =>
+        item.UserEmail === loggedInUserEmail &&
+        !(
+          item.UserEmail === loggedInUserEmail &&
+          item.Email === loggedInUserEmail
+        )
+    ).length === 0;
   useEffect(() => {
     setSelfExpense(false);
   }, []);
@@ -68,73 +85,97 @@ export default function SplitBill({
       >
         <Header selectPersonList={selectPersonList} lang={lang} theme={theme} />
         <div className="flex flex-col items-center justify-center">
-          <ul className="m-0 py-1 w-3/4">
-            {/* Table generator for people added */}
-            {dataThrow
-              .filter(
-                (item) =>
-                  item.UserEmail === loggedInUserEmail &&
-                  !(
-                    item.UserEmail === loggedInUserEmail &&
-                    item.Email === loggedInUserEmail
-                  )
-              )
-              .map((item, index) => (
-                <React.Fragment key={index}>
-                  {item.Name ? (
-                    <Link
-                      to={`/App/ReceiptInput/${item.ContactId}`}
-                      onClick={() => selectPerson(item.ContactId)}
-                      className="no-underline py-1"
-                    >
-                      <li
-                        className={
-                          "list-group-item flex justify-between m-1 p-2 rounded-lg shadow-sm " +
-                          (theme === "dark"
-                            ? "bg-gray-800 text-white"
-                            : "bg-white text-gray-800")
-                        }
-                      >
-                        <div className="flex items-center">
-                          <Avatar name={item.Name} size={32} round />
-                          <span className="ml-1">
-                            {item.Name.length > 8
-                              ? `${item.Name.substring(0, 8)}...`
-                              : item.Name}
-                          </span>
-                        </div>
-                        <span
-                          className={`badge badge-pill rounded px-1 pt-2 ml-2 text-xs ${
-                            parseFloat(item.Owing) < 0
-                              ? "bg-red-500 text-black"
-                              : "bg-blue-500 text-white"
-                          }`}
-                        >
-                          ${parseFloat(item.Owing).toFixed(2)}
-                        </span>
-                      </li>
-                    </Link>
-                  ) : null}
-                </React.Fragment>
-              ))}
-          </ul>
-
-          <Button
-            variant="gradient"
-            className="gradient-btn mb-2 flex items-center justify-center"
-            style={{ margin: "auto" }}
-            onClick={() => {
-              setAddPerson(true);
-              setFormSubmitted(true);
-            }}
-          >
-            <div className="flex items-center">
-              <IoPersonAddSharp size={24} />
-              <span className="text-white ml-2">
-                {lang === "english" ? "Add Person" : "Ajouter Une Personne"}
-              </span>
+          {isEmptyDataThrow ? (
+            <div className="mb-5">
+              <Button
+                variant="gradient"
+                className="gradient-btn mb-2 flex items-center justify-center glow pulsing"
+                style={{ margin: "auto" }}
+                onClick={() => {
+                  setAddPerson(true);
+                  setFormSubmitted(true);
+                }}
+              >
+                <div className="flex items-center">
+                  <IoPersonAddSharp size={24} />
+                  <span className="text-white ml-2">
+                    {lang === "english" ? "Add Person" : "Ajouter Une Personne"}
+                  </span>
+                </div>
+              </Button>
             </div>
-          </Button>
+          ) : (
+            <>
+              <ul className="max-w-md mb-3 p-2 divide-y divide-gray-200 dark:divide-gray-700 bg-gray-800 dark:bg-gray-900 rounded-lg overflow-hidden shadow-lg">
+                {dataThrow
+                  .filter(
+                    (item) =>
+                      item.UserEmail === loggedInUserEmail &&
+                      !(
+                        item.UserEmail === loggedInUserEmail &&
+                        item.Email === loggedInUserEmail
+                      )
+                  )
+                  .map((item, index) => (
+                    <li key={index} className="py-3 sm:py-4">
+                      <Link
+                        onClick={() => {
+                          selectPerson(item.ContactId);
+                          setShowMethodSelectModal(true);
+                          setSelectedModalPerson(item.ContactId);
+                        }}
+                        className="no-underline"
+                      >
+                        <div className="flex items-center space-x-4 rtl:space-x-reverse">
+                          <div className="flex-shrink-0">
+                            <Avatar name={item.Name} size={32} round />
+                          </div>
+                          <div className="flex-1 min-w-0">
+                            <p className="text-sm font-medium text-gray-100 truncate dark:text-white">
+                              {item.Name}
+                            </p>
+                            <p className="text-sm text-gray-400 truncate dark:text-gray-400">
+                              {item.Email.length > 15
+                                ? `${item.Email.substring(0, 15)}...`
+                                : item.Email}
+                            </p>
+                          </div>
+                          <div
+                            className={`inline-flex items-center text-base font-semibold ${
+                              parseFloat(item.Owing) < 0
+                                ? "text-red-500"
+                                : "text-blue-500"
+                            }`}
+                          >
+                            ${parseFloat(item.Owing).toFixed(2)}
+                          </div>
+                        </div>
+                      </Link>
+                    </li>
+                  ))}
+              </ul>
+              <div className="mb-5">
+                <Button
+                  variant="gradient"
+                  className="gradient-btn mb-2 flex items-center justify-center"
+                  style={{ margin: "auto" }}
+                  onClick={() => {
+                    setAddPerson(true);
+                    setFormSubmitted(true);
+                  }}
+                >
+                  <div className="flex items-center">
+                    <IoPersonAddSharp size={24} />
+                    <span className="text-white ml-2">
+                      {lang === "english"
+                        ? "Add Person"
+                        : "Ajouter Une Personne"}
+                    </span>
+                  </div>
+                </Button>
+              </div>
+            </>
+          )}
         </div>
 
         <CSSTransition
@@ -171,6 +212,34 @@ export default function SplitBill({
           ></AddPerson>
         </CSSTransition>
       </main>
+      {showMethodSelectModal && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white p-6 rounded-lg">
+            <MethodSelect
+              lang={lang}
+              setSelectMethodManual={setSelectMethodManual}
+              setSelectMethodPicture={setSelectMethodPicture}
+              setSelectPersonReceipt={setSelectPersonReceipt}
+              setPersonReceiptAmount={setPersonReceiptAmount}
+              selectedModalPerson={selectedModalPerson}
+              selectMethodPicture={selectMethodPicture}
+              selectMethodManual={selectMethodManual}
+            />
+            <Button
+              variant="gradient"
+              className="gradient-btn mb-2 flex items-center justify-center"
+              style={{ margin: "auto" }}
+              onClick={() => setShowMethodSelectModal(false)}
+            >
+              <div className="flex items-center">
+                <span className="text-white">
+                  {lang === "english" ? "Close" : "Fermer"}
+                </span>
+              </div>
+            </Button>
+          </div>
+        </div>
+      )}
     </>
   );
 }
